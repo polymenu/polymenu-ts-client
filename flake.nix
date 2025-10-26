577a99bec575b5988fcc6012011c2d5a046cd1b3
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2025-10-25 21:46:38 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    withSystem = f:
+      lib.fold lib.recursiveUpdate {}
+      (map f ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]);
+  in
+    withSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.${system}.default =
+          pkgs.mkShell
+          {
+            packages = with pkgs; [
+              nodePackages.pnpm
+              nodejs
+              deno
+            ];
+          };
+      }
+    );
+}
